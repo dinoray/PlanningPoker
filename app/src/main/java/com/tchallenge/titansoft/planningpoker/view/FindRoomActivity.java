e64954801a12120ca58942e9adf7b5c892e4181e
Kyo Lai Hsueh-Cheng <kyo.lai82@titansoft.com.sg>
2017-10-13 10:29:11 +0800

    [implement] 1.code format by ctrl+alt+L 2. auto gen number while create room

--- app/src/main/java/com/tchallenge/titansoft/planningpoker/view/FindRoomActivity.java
+++ app/src/main/java/com/tchallenge/titansoft/planningpoker/view/FindRoomActivity.java
@@ -8,10 +8,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.tchallenge.titansoft.planningpoker.R;
 import com.tchallenge.titansoft.planningpoker.contract.FindRoomContract;
 import com.tchallenge.titansoft.planningpoker.prsenter.FindRoomPresenter;
+
+import java.util.Random;
 
 public class FindRoomActivity extends AppCompatActivity implements FindRoomContract.IFinRoomView {
 
@@ -26,27 +29,31 @@ public class FindRoomActivity extends AppCompatActivity implements FindRoomContr
     }
 
     private void initControl(final FindRoomContract.IFindRoomPresenter findRoomPresenter) {
-        int typeInt = getIntent().getIntExtra("Type",0);
+        int typeInt = getIntent().getIntExtra("Type", 0);
 
-        if (typeInt == 1001){
+        if (typeInt == 1001) {
             Button createBtn = (Button) findViewById(R.id.btn_create);
             createBtn.setVisibility(View.VISIBLE);
             createBtn.setOnClickListener(new android.view.View.OnClickListener() {
                 @Override
                 public void onClick(android.view.View view) {
-                    String pincode = ((EditText)findViewById(R.id.edit_pincode)).getText().toString();
-                    String nickname = ((EditText)findViewById(R.id.edit_nickname)).getText().toString();
+                    String pincode = ((EditText) findViewById(R.id.edit_pincode)).getText().toString();
+                    String nickname = ((EditText) findViewById(R.id.edit_nickname)).getText().toString();
                     findRoomPresenter.create(pincode, nickname);
                 }
             });
-        } else if(typeInt == 1002){
+            EditText pinCodeEditTxt = (EditText) findViewById(R.id.edit_pincode);
+            Random rnd = new Random();
+            int n = 1000 + rnd.nextInt(9000);
+            pinCodeEditTxt.setText(String.valueOf(n));
+        } else if (typeInt == 1002) {
             Button joinBtn = (Button) findViewById(R.id.btn_join);
             joinBtn.setVisibility(View.VISIBLE);
             joinBtn.setOnClickListener(new android.view.View.OnClickListener() {
                 @Override
                 public void onClick(android.view.View view) {
-                    String pincode = ((EditText)findViewById(R.id.edit_pincode)).getText().toString();
-                    String nickname = ((EditText)findViewById(R.id.edit_nickname)).getText().toString();
+                    String pincode = ((EditText) findViewById(R.id.edit_pincode)).getText().toString();
+                    String nickname = ((EditText) findViewById(R.id.edit_nickname)).getText().toString();
                     findRoomPresenter.join(pincode, nickname);
                 }
             });
@@ -54,8 +61,8 @@ public class FindRoomActivity extends AppCompatActivity implements FindRoomContr
     }
 
     @Override
-    public void startGame(){
-        Intent intent = new Intent(this,SelectCardActivity.class);
+    public void startGame() {
+        Intent intent = new Intent(this, SelectCardActivity.class);
         startActivity(intent);
         finish();
     }
@@ -65,7 +72,7 @@ public class FindRoomActivity extends AppCompatActivity implements FindRoomContr
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle(R.string.activity_find_room_error_title);
         builder.setMessage(R.string.activity_find_room_error_message);
-        builder.setPositiveButton(R.string.dialog_btn_ok, new DialogInterface.OnClickListener(){
+        builder.setPositiveButton(R.string.dialog_btn_ok, new DialogInterface.OnClickListener() {
 
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
